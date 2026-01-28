9c91785cb87f710dcb92b3c29501232d864b3234
Shahul Mubeen A <a.shahul.mubeen@gmail.com>
2026-01-28 11:57:32 +0000

    Added testing for SheetManager

---
+++ tests/test_sheet_manager.py
@@ -0,0 +1,16 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from src.gmail_client import GmailClient
+from src.email_summerizer import EmailSummarizer
+from src.sheet_manager import SheetManager
+
+client = GmailClient(credentials_path='credentials.json')
+summarizer = EmailSummarizer(model='deepseek-r1:8b')
+sheet_manager = SheetManager(sheet_path='tests/output/applications.xlsx')
+
+emails = client.fetch_emails(days=1, query='application')
+extracted_data = summarizer.summarize_emails(emails[0:5])  # Summarize first 5 emails for testing
+sheet_path = sheet_manager.write_to_excel(extracted_data)
