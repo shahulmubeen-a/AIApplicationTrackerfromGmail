77a894b1da5f2f8cf22589fe207fd15d251b3939
Shahul Mubeen A <a.shahul.mubeen@gmail.com>
2026-01-28 12:05:25 +0000

    Changed argument name to update with sheet_manager.py

--- tests/test_sheet_manager.py
+++ tests/test_sheet_manager.py
@@ -9,7 +9,7 @@ from src.sheet_manager import SheetManager
 
 client = GmailClient(credentials_path='credentials.json')
 summarizer = EmailSummarizer(model='deepseek-r1:8b')
-sheet_manager = SheetManager(sheet_path='tests/output/applications.xlsx')
+sheet_manager = SheetManager(output_path ='tests/output/applications.xlsx')
 
 emails = client.fetch_emails(days=1, query='application')
 extracted_data = summarizer.summarize_emails(emails[0:5])  # Summarize first 5 emails for testing
